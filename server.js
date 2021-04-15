45cdbfd97df2ed01e2e9ea016b0d2e662f326a95
Asia Alnahi <asia.alius@gmail.com>
2021-04-15 14:10:16 -0700

    add routing

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const notes = [
-    {
-    noteTitle: '',
-    noteText: '' 
-    }
+const notesData = [
+    
 ];
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html'))); 
 
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
+
+app.get('/api/notes/:id', (req,res) => {
+    const newNote = req.params.id;
+    console.log(newNote);
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
